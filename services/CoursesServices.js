5236f994d28a551e77dd91999d381243cca7e069
Rebecca Grenet <rebeccagnt@orange.fr>
2023-04-08 22:45:54 +0200

    add comment services

--- services/CoursesServices.js
+++ services/CoursesServices.js
@@ -4,13 +4,16 @@ export const CoursesServices = {
   //Récupère la liste de tous les cours
   async fetchCourses() {
     try {
+      // Récupération de la collection des cours sur Firebase
       const coursesCollection = firebase.db.collection("courses");
       const snapshot = await coursesCollection.get();
+      // Map des données de chaque document et stock de l'id du document dans l'objet course
       const coursesList = snapshot.docs.map((doc) => {
         const course = doc.data();
         course.id = doc.id;
         return course;
       });
+      // Retourne la liste des objets "course"
       return coursesList;
     } catch (error) {
       console.error(error);
@@ -21,14 +24,17 @@ export const CoursesServices = {
   //Récupère la liste des 3 meilleurs cours
   async fetchBestsCourses() {
     try {
+      // Récupération de la collection des cours sur Firebase
       const coursesCollection = firebase.db.collection("courses");
+
+      // Récupération de la collection triée par ordre décroissant en fonction du nombre de lectures
       const snapshot = await coursesCollection
         .orderBy("nbReadings", "desc")
         .limit(3)
         .get();
       const bestCoursesList = snapshot.docs.map((doc) => {
         const course = doc.data();
-        course.id = doc.id;
+        course.id = doc.id; // Ajout de l'id du document à l'objet course
         return course;
       });
       return bestCoursesList;
@@ -40,13 +46,26 @@ export const CoursesServices = {
 
   // Récupère la liste des cours associés au nom "name" entré par l'utilisateur
   async searchCourses(name) {
-    const coursesCollection = firebase.db.collection("courses");
-    const snapshot = await coursesCollection.where("name", "==", name).get();
-    const coursesList = snapshot.docs.map((doc) => {
-      const course = doc.data();
-      course.id = doc.id;
-      return course;
-    });
-    return coursesList;
+    try {
+      // Récupération de la collection des cours sur Firebase
+      const coursesCollection = firebase.db.collection("courses");
+
+      // Requête avec le champ "name" de la collection des cours
+      const snapshot = await coursesCollection.where("name", "==", name).get();
+
+      //Création d'une liste de cours à partir des documents retournés
+      const coursesList = snapshot.docs.map((doc) => {
+        const course = doc.data();
+        course.id = doc.id;
+        return course;
+      });
+      //Retourne la liste de cours trouvés
+      return coursesList;
+    } catch (error) {
+      console.error(error);
+      throw new Error(
+        "Erreur dans la récupération des cours en fonction de la recherche"
+      );
+    }
   },
 };
--- services/QuizzesServices.js
+++ services/QuizzesServices.js
@@ -1,20 +1,24 @@
 import firebase from "./firebaseConfig.js";
 
 export const QuizzesServices = {
-    //Récupération des questions du quiz en fonction de l'ID de la séquence
-    async fetchQuestionsForQuiz(quizId) {
-        const quizRef = firebase.db.collection("quizzes").doc(quizId);
-        const doc = await quizRef.get();
-        if (!doc.exists) {
-            // Le quiz n'a pas été trouvé
-            return null;
-        }
-        const questionsRef = firebase.db
-            .collection("questions")
-            .where("quiz", "==", quizRef);
-        const questionsSnapshot = await questionsRef.get();
-        const questions = questionsSnapshot.docs.map((doc) => doc.data());
+  //Récupération des questions du quiz en fonction de l'ID de la séquence
+  async fetchQuestionsForQuiz(quizId) {
+    // Récupération de la référence du workflow avec l'id
+    const quizRef = firebase.db.collection("quizzes").doc(quizId);
+    const doc = await quizRef.get();
+    if (!doc.exists) {
+      // Le quiz n'a pas été trouvé
+      return null;
+    }
+    // Récupération de toutes les questions qui ont pour référence le document quiz
+    const questionsRef = firebase.db
+      .collection("questions")
+      .where("quiz", "==", quizRef);
+    const questionsSnapshot = await questionsRef.get();
 
-        return questions;
-    },
+    // Map des données de chaque document question dans un tableau de questions
+    const questions = questionsSnapshot.docs.map((doc) => doc.data());
+
+    return questions;
+  },
 };
